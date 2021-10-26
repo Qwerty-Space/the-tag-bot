0576db3c24fad372123841b8db0f02cf144f7e22
udf <tabhooked@gmail.com>
2021-10-26 21:31:23 +0200

    replace lark with custom parser

--- query_parser.py
+++ query_parser.py
@@ -1,93 +1,148 @@
+import regex
 from dataclasses import dataclass
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
-from lark import Lark, Transformer, v_args
-import emoji
+from emoji import UNICODE_EMOJI_ENGLISH
 
 from utils import MediaTypes
+
+EMOJI_MODIFIERS = set([
+  # Skin tones
+  '\U0001f3fb', '\U0001f3fc', '\U0001f3fd', '\U0001f3fe', '\U0001f3ff',
+  # Joiner
+  '\u200d'
+])
+
+# Emoji without any skintones or joins
+PLAIN_EMOJI = set(
+  c.strip('\ufe0f') for c in UNICODE_EMOJI_ENGLISH
+  if not (set(c) & EMOJI_MODIFIERS)
+)
+
+
+def strip_emojis(text):
+  """
+  Strips all emojis from text, returns cleaned text and each "simple" emoji
+  (splits joined emoji, strips skintones, etc)
+  P.S: I hate Unicode
+  """
+  emojis = {}
+  def emoji_repl(m):
+    grapheme = m.group(0)
+
+    if grapheme in PLAIN_EMOJI:
+      emojis[grapheme] = None
+      return ''
+
+    has_emoji = False
+    for c in grapheme:
+      if c in PLAIN_EMOJI:
+        emojis[c] = None
+        has_emoji = True
+    return '' if has_emoji else grapheme
+
+  clean_text = regex.sub(r'\X', emoji_repl, text)
+  return clean_text, list(emojis.keys())
+
+
+@dataclass
+class ParseWarning:
+  name: str
+  data: str
+  pos: int = -1
 
 
 @dataclass
 class Field:
   name: str
-  content: list[str]
-  is_neg: bool
+  aliases: list[str]
+  is_short: bool = False
+  allow_negation: bool = True
 
 
-@dataclass
-class FieldError:
-  name: str
-  data: str
-  column: int = -1
-  end_column: int = -1
+FieldKey = namedtuple('FieldKey', ['name', 'is_neg'])
 
 
-class QueryFlattener(Transformer):
-  DEFAULT_FIELD = 'tags'
+FIELDS = [
+  Field('tags', ['s']),
+  Field('file_name', ['fn']),
+  Field('ext', ['ext', 'e']),
+  Field('pack_name', ['pack', 'p']),
+  Field('type', ['type', 't'], is_short=True, allow_negation=False),
+  Field('animated', ['animated'], is_short=True)
+]
 
-  def WORD(self, token):
-    return token.value
-
-  def _parse_word(self, t, is_neg):
-    return Field(self.DEFAULT_FIELD, t, is_neg)
-
-  word = lambda self, t: self._parse_word(t, False)
-  word_neg = lambda self, t: self._parse_word(t, True)
-
-  @v_args(meta=True)
-  def error_unknown_field(self, t, meta):
-    return FieldError('Unknown field', t[0], meta.column, meta.end_column)
-
-  @v_args(meta=True)
-  def error_no_value(self, t, meta):
-    return FieldError('No value found', t[0].data, meta.column, meta.end_column)
-
-  @v_args(meta=True)
-  def error_stray_colon(self, t, meta):
-    return FieldError('Stray colon', '', meta.column, meta.end_column)
-
-  def _parse_field(self, t, is_neg):
-    if isinstance(t[0], FieldError):
-      return t[0]
-    field_name = t[0].data
-    return Field(field_name, t[1:], is_neg)
-
-  field = lambda self, t: self._parse_field(t, False)
-  field_neg = lambda self, t: self._parse_field(t, True)
-
-  def query(self, t):
-    return t
-
-
-with open('query.lark') as f:
-  query_parser = Lark(
-    f.read(),
-    start='query',
-    parser='lalr',
-    propagate_positions=True
-  )
-
-query_flattener = QueryFlattener()
+ALIAS_TO_FIELD = {
+  alias: field
+  for field in FIELDS
+  for alias in field.aliases
+}
 
 
 def parse_query(query):
-  fields = query_flattener.transform(query_parser.parse(query))
-  positive_fields = defaultdict(list)
-  negative_fields = defaultdict(list)
-  errors = defaultdict(list)
+  def set_current_field(field, span, is_neg=False):
+    nonlocal current_field, current_field_start, negated_field, field_was_used
+    if not field_was_used:
+      warnings.append(ParseWarning(
+        'Field unused',
+        current_field.name,
+        current_field_start
+      ))
+    current_field = field
+    current_field_start = span[0]
+    negated_field = field.allow_negation and bool(is_neg)
+    field_was_used = False
 
-  for field in fields:
-    if isinstance(field, FieldError):
-      errors[field.name].append(field)
+  fields = defaultdict(list)
+  warnings = []
+
+  default_field = ALIAS_TO_FIELD.get('s')
+
+  current_field = default_field
+  current_field_start = 0
+  negated_field = False
+  field_was_used = True
+  for m in regex.finditer(r'(?P<is_neg>[\!-]*)(?P<token>[^\s:]+)(?P<is_field>:?)|(\n)', query):
+    token = m.group('token')
+    token_is_neg = bool(m.group('is_neg'))
+
+    # Newlines reset the field
+    if m.group(0) == '\n':
+      set_current_field(default_field, m.span())
+      # prevent warning if field is changed
+      field_was_used = True
       continue
-    d = negative_fields if field.is_neg else positive_fields
-    name = field.name
-    # TODO: extract emoji
-    d[name].extend(field.content)
 
-  # TODO: remove negative/multiple types
+    if m.group('is_field'):
+      token = token.lower()
+      field = ALIAS_TO_FIELD.get(token)
+      if not field:
+        warnings.append(ParseWarning('Unknown field', token, m.span()[0]))
+        continue
+      set_current_field(field, m.span(), token_is_neg)
+      continue
 
-  return positive_fields, negative_fields, errors
+    token, emojis = strip_emojis(token)
+    key = FieldKey('emoji', token_is_neg)
+    for emoji in emojis:
+      fields[key].append(emoji)
 
-r = parse_query('t:sticker 💀 mugi p:p:bob k-on\n!yui !animated:yes bla:bla !fn:sticker.webp :partial :')
-print(r)
+    if not token:
+      continue
+
+    is_neg = negated_field ^ token_is_neg
+    fields[FieldKey(current_field.name, is_neg)].append(token)
+    field_was_used = True
+    if current_field.is_short:
+      current_field = default_field
+
+  return fields, warnings
+
+
+fields, warnings = parse_query('t:sticker 💀✌🏿🇺🇸mugi !2️⃣ p:p:bob k-on\n!yui !animated:yes bla:bla !fn:sticker.webp :weed:')
+
+for field, data in fields.items():
+  print(field, data)
+
+for warning in warnings:
+  print(warning)
